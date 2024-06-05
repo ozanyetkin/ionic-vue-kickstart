4b227994582474f033c8a72f2c12029b87e9a1ab
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-06-05 16:02:10 +0300

    motion sensor permission error fixed, camera tbd

--- develad-app/src/views/Tab2Page.vue
+++ develad-app/src/views/Tab2Page.vue
@@ -12,14 +12,30 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Tab 2 page" />
 
-      <iframe src="https://webar.imaginerealities.com.au/wt/demo/" width="100%" height="500px"></iframe>
+      <iframe src="https://webar.imaginerealities.com.au/wt/demo/" width="100%"
+        height="800px"></iframe>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { ref, onMounted } from 'vue';
+import { Motion } from '@capacitor/motion';
+
+const hasPermission = ref(false);
+
+onMounted(async () => {
+  try {
+    await DeviceMotionEvent.requestPermission();
+    hasPermission.value = true;
+    Motion.addListener('accel', event => {
+      console.log('Device motion event:', event);
+    });
+  } catch (error) {
+    console.error('Motion permission not granted:', error);
+  }
+});
 </script>
